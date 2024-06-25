ac9b8e94b998bd6864338f1ad9ffe4cc41bafc6e
GuilhermeAilton <145512980+GuilhermeAilton@users.noreply.github.com>
2024-06-25 15:39:33 -0300

    Add files via upload

---
+++ controle_de_fluxo.js
@@ -0,0 +1,64 @@
+const readline = require('readline-sync')
+
+
+// let opcao = readline.questionInt('Digite: 1 - Cadastrar e 2 - Sair >>');
+
+
+// switch(opcao) {
+//     case 1:
+//         console.log("opcao 1")
+        
+//     case 2: 
+//         console.log('opcao 2')
+      
+//     case 3 :
+//         console.log('opcao 3') 
+        
+
+// }
+
+// let idade = 18;
+// let aviso;
+
+// aviso = (idade>=18) ? (aviso = "Voce pode jogar") : (aviso = 'Voce não pode jogar')
+
+// console.log(aviso)
+
+// let opcao = readline.questionInt('Digite: 1 - Cadastrar e 2 - Sair >>');
+// let escolha;
+// escolha = (opcao == '1') ? (escolha = 'Cadastrar') : (escolha = 'Sair')
+
+
+// console.log(escolha)
+
+
+
+// let i = 0;
+
+
+// while (i < 5) {
+//     console.log('oi');
+
+//     i++;
+ 
+// }
+
+
+// opcao =  1 ;
+
+
+// while (opcao == 1) {
+//      opcao = readline.questionInt('Digite: 1 - Cadastrar e 2 - Sair >>');
+//     console.log('Cadastrar')
+    
+// }
+
+// console.log('Progrma finalizado com sucesso')
+
+
+
+// i = 0 
+
+// do {
+//     console.log('Bem vindo ao Fap')
+// }while(i <= 0)
