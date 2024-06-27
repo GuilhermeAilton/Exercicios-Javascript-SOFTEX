a5792f874caae7b8be69d255a50c819e7970a87f
GuilhermeAilton <145512980+GuilhermeAilton@users.noreply.github.com>
2024-06-27 15:59:28 -0300

    Add files via upload

--- funcao.js
+++ funcao.js
@@ -9,46 +9,61 @@
 
 const readline = require ('./node_modules/readline-sync')
 
-let num1 = readline.questionFloat('Digite o primeiro numero : ')
-let num2 = readline.questionFloat('Digite o segundo numero : ')
+// let num1 = readline.questionFloat('Digite o primeiro numero : ')
+// let num2 = readline.questionFloat('Digite o segundo numero : ')
 
-console.log('')
+// console.log('')
 
 
 function soma(num1, num2) {
     let resultado = num1 + num2
-    return resultado
-    
+    console.log(`O resultado da soma : ${resultado}`)
+  
+  
 }
 
 
-function subtracao(num1, num2) {
-    let resultado = num1 - num2
-    return resultado
+// function subtracao(num1, num2) {
+//     let resultado = num1 - num2
+//     console.log(`O resultado da subracao : ${resultado}`)
     
-}
+// }
 
 
-function divisao(num1, num2) {
-    let resultado = num1 / num2
-    return resultado.toFixed(2)
+// function divisao(num1, num2) {
+//     let resultado = num1 / num2
+//     console.log(`O resultado da divisao : ${resultado}`)
     
-}
+// }
 
-function multiplicao(num1, num2) {
-    let resultado = num1 * num2
-    return resultado
+// function multiplicao(num1, num2) {
+//     let resultado = num1 * num2
+//     console.log(`O resultado da multiplicao : ${resultado}`)
     
+// }
+
+
+
+// soma(num1,num2)
+// console.log("")
+// subtracao(num1,num2)
+// console.log("")
+// divisao(num1,num2)
+// console.log("")
+// multiplicao(num1,num2)
+
+
+
+
+// LOOP PARA PROGRAMA DE OPERAÇÕES
+let i = 1 ;
+
+while(i == 1 ) {
+    let num1 = readline.questionFloat('Digite o primeiro numero : ')
+    let num2 = readline.questionFloat('Digite o segundo numero : ')
+    soma(num1,num2)
+
+    i = readline.questionInt('Digite 1 para continuar \n -- 2 para sair  :')
+
 }
-
-
-
-console.log(`O resultado da soma : ${soma(num1,num2)}`)
-console.log("")
-console.log(`O resultado da subtracao : ${subtracao(num1,num2)}`)
-console.log("")
-console.log(`O resultado da divisão : ${divisao(num1,num2)}`)
-console.log("")
-console.log(`O resultado da multiplicao : ${multiplicao(num1,num2)}`)
-
-
+console.log('-------- Fim do programa ---------');
