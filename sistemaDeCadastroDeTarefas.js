f94780004167bb9ff01d341b3f435fef1444cb3f
GuilhermeAilton <145512980+GuilhermeAilton@users.noreply.github.com>
2024-07-02 20:29:37 -0300

    sistemaDeCadastroDeTarefas.js
    CRUD em Java script  utilizando array

---
+++ sistemaDeCadastroDeTarefas.js
@@ -0,0 +1,129 @@
+const readline = require('readline-sync')
+//pacote para adionar cores ao terminal
+const chalk = require('chalk')
+let listaDeTarefas = [];
+
+let entrada = 1;
+while (entrada != 5) {
+  console.log('')
+  console.log(chalk.bgGreen("=====  MENU ====="));
+  console.log("");
+
+  console.log("1 - Cadastrar Tarefa");
+  console.log("2 - Alterar uma tarefa ");
+  console.log("3 - Excluir uma tarefa");
+  console.log("4 - Listar tarefas ");
+  console.log("5 - Sair do sistema");
+  console.log("");
+
+  let opcao = readline.questionInt("ESCOLHA UMA OPCAO : ");
+
+  console.log("");
+  console.log("");
+
+  if (opcao == 1) {
+    let adicionarTarefa = readline.question( " Insira uma tarefa  :  "  );
+    while (adicionarTarefa.trim() === '') {
+      console.log('')
+      console.log('')
+      console.log('******************');
+      console.info(chalk.yellow('Tarefa inválida'));
+      console.log('******************');
+      console.log('')
+      adicionarTarefa = readline.question("Insira uma tarefa :  ");
+    }
+    listaDeTarefas.push(adicionarTarefa)
+    console.log('')
+    console.log('')
+    console.log(chalk.green('==================='));
+    console.log(chalk.green('Tarefa cadastrada com sucesso'));
+    console.log(chalk.green('==================='));
+    console.log('')
+
+  } 
+  else if (opcao == 2) {
+        // Verifica se a lista de tarefas está vazia
+        if (listaDeTarefas.length == 0){
+          console.log(chalk.bgYellowBright('A lista de Tarefas está vazia '))
+          console.log('')
+          console.log(chalk.bgYellowBright('Cadastre uma nova tarefa '))
+          console.log("")
+
+        }else{
+              // EXIBE A LISTA DE TAREFAS 
+              cont = 1
+              console.log('=========== Lista de Tarefas =========== ')
+              for(i=0; i < listaDeTarefas.length; i++) {
+              
+                console.log('')
+                console.log(chalk.green(`${cont} - ${listaDeTarefas[i]}`))
+                cont +=1
+                console.log("----------------------------------------");
+               
+              }
+              console.log('')
+              // REQUER A ENTRADA DO DADO DO USUÁRIO PARA ESCOLHER A TAREFA DE ACORDO COM NÚMERO
+              let alterarTarefa = readline.question( "Digite o numero da tarefa que deseja alterá-la :  "  );
+              var index = listaDeTarefas.indexOf(alterarTarefa);
+              index+=1
+              if (index > -1) {
+                listaDeTarefas[index] = readline.question('Descreva a nova tarefa : ')
+                console.log('')
+                console.log(chalk.greenBright('********* Tarefa Alterada com sucesso *********** '))
+    1
+              }
+              
+        }
+ 
+  } 
+  
+  else if (opcao == 3) {
+    cont = 1
+    console.log('=========== Lista de Tarefas =========== ')
+    for(i=0; i < listaDeTarefas.length; i++) {
+    
+      console.log('')
+      console.log(chalk.green(`${cont} - ${listaDeTarefas[i]}`))
+      cont +=1
+      console.log("----------------------------------------");
+     
+    }
+    console.log('')
+    // REQUER A ENTRADA DO DADO DO USUÁRIO PARA ESCOLHER A TAREFA DE ACORDO COM NÚMERO
+    let alterarTarefa = readline.question( "Digite o numero da tarefa que deseja apagar :  "  );
+    var index = listaDeTarefas.indexOf(alterarTarefa);
+    index+=1
+    if (index > -1) {
+      listaDeTarefas.splice(index,1)
+      console.log('')
+      console.log(chalk.bgRed('********* Tarefa exluida com sucesso *********** '))
+1
+    }
+    
+  }
+  else if (opcao == 4){
+
+    if (listaDeTarefas.length == 0){
+      console.log(chalk.bgYellowBright('NÃO HÁ TAREFAS NA LISTA PARA SEREM EXIBIDAS '))
+      console.log('')
+
+    }else{
+          // EXIBE A LISTA DE TAREFAS 
+          cont = 1
+          console.log('=========== Lista de Tarefas =========== ')
+          for(i=0; i < listaDeTarefas.length; i++) {
+          
+            console.log('')
+            console.log(chalk.green(`${cont} - ${listaDeTarefas[i]}`))
+            cont +=1
+            console.log("----------------------------------------");
+          } 
+     }
+}
+  else if (opcao == 5) {
+    entrada = 5
+
+  }
+}
+console.log("--------- FIM DO PROGRAMA ----------------");
+c
